cbb45f43b1768c6736bcd11e64e01decd11f6d40
Juan Uribe <juanjouribev1@gmail.com>
2020-05-29 13:00:49 -0700

    Improve style of overlay.js

--- portfolio/src/main/webapp/scripts/overlay.js
+++ portfolio/src/main/webapp/scripts/overlay.js
@@ -5,10 +5,10 @@ let activeOverlay = null;
  * Decides wether to show or hide the overlay.
  */
 function toggleOverlay(section){
-  if(isShowingOverlay) (isShowingOverlay =  hideOverlay(activeOverlay));
+  if(isShowingOverlay) (hideOverlay(activeOverlay));
   if(activeOverlay === section) (activeOverlay = null);
   else{
-    isShowingOverlay = showOverlay(section);
+    showOverlay(section);
     activeOverlay = section;
   }
 };
@@ -17,16 +17,25 @@ function toggleOverlay(section){
  * Shows the overlay of the specified section.
  */
 function showOverlay(section){
-  let overlay = document.querySelector(`#${section} .overlay`);
-  overlay.style.opacity = "1";
-  return true;
+  changeOverlayOpacity(section, "1");
+  isShowingOverlay = true;
+  return;
 };
 
 /**
  * Hides the overlay of the specified section.
  */
 function hideOverlay(section){
+  changeOverlayOpacity(section, "0");
+  isShowingOverlay = false;
+  return;
+};
+
+/**
+ * Cahnges the opacity of the overlay.
+ */
+function changeOverlayOpacity(section, opacity){
   let overlay = document.querySelector(`#${section} .overlay`);
-  overlay.style.opacity = "0";
-  return false;
+  overlay.style.opacity = opacity;
+  return;
 };
