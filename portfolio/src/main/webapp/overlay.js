6b070b6497114fe2b17b028217ec8178c16d3ae8
Juan Uribe <juanjouribev1@gmail.com>
2020-05-29 12:38:05 -0700

    Add file to keep overlay over manu item when clicked

---
+++ portfolio/src/main/webapp/overlay.js
@@ -0,0 +1,38 @@
+// const options = document.querySelectorAll(".menu-option");
+
+for(let i=0; i<options.length; i++){
+  options[i].addEventListener('click', function(){toggleOverlay(options[i].id)})
+};
+
+let isShowingOverlay = false;
+let activeOverlay = null;
+
+/**
+ * Decides wether to show or hide the overlay.
+ */
+function toggleOverlay(section){
+  if(isShowingOverlay) (isShowingOverlay =  hideOverlay(section));
+  if(activeOverlay === section) (activeOverlay = null);
+  else{
+    isShowingOverlay = showOverlay(section);
+    activeOverlay = section;
+  }
+};
+
+/**
+ * Shows the overlay of the specified section.
+ */
+function showOverlay(section){
+  let overlay = document.querySelector(`#${section} .overlay`);
+  overlay.style.opacity = "1";
+  return true;
+};
+
+/**
+ * Hides the overlay of the specified section.
+ */
+function hideOverlay(section){
+  let overlay = document.querySelector(`#${section} .overlay`);
+  overlay.style.opacity = "0";
+  return false;
+};
