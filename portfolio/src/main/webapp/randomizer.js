06b60b04beee437aba6fa605d2f8a5f129a8dae8
Juan Uribe <juanjouribev1@gmail.com>
2020-05-29 12:10:28 -0700

    Improve javascript style, and removed javascript from html

--- portfolio/src/main/webapp/randomizer.js
+++ portfolio/src/main/webapp/randomizer.js
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const factButton = document.querySelector("#fact-button");
+
+factButton.addEventListener('click', addRandomFact);
+
 /**
- * Adds a random greeting to the page.
+ * Adds a random fact to the page.
  */
 function addRandomFact() {
   const facts =
--- portfolio/src/main/webapp/toggle.js
+++ portfolio/src/main/webapp/toggle.js
@@ -1,6 +1,15 @@
+const options = document.querySelectorAll(".menu-option");
+
+for(let i=0; i<options.length; i++){
+  options[i].addEventListener('click', function(){toggleSection(options[i].id)})
+};
+
 let isShowing = false;
 let activeSection = null;
 
+/**
+ * Decides wether to load or hide the section.
+ */
 function toggleSection(section){
   if(isShowing) (isShowing =  hideSection());
   if(activeSection === section) (activeSection = null);
@@ -10,11 +19,17 @@ function toggleSection(section){
   }
 };
 
+/**
+ * Loads the specified section.
+ */
 function loadSection(section){
   $("#option-result").load(`${section}.html`); 
   return true;
 };
 
+/**
+ * Hides the activity section.
+ */
 function hideSection(){
   $("#option-result").empty();
   return false;
