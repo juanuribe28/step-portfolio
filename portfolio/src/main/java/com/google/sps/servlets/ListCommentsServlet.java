b23afd7acb52c757edb68862be693645c4fbc9af
Juan Uribe <juanjouribev1@gmail.com>
2020-06-10 16:11:38 +0000

    Remove solved TODO comment

--- portfolio/src/main/java/com/google/sps/servlets/ListCommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ListCommentsServlet.java
@@ -45,7 +45,6 @@ public class ListCommentsServlet extends HttpServlet {
 
     SortDirection sortingDirection = sortingDirectionString.equals("descending") ? SortDirection.DESCENDING : SortDirection.ASCENDING;
 
-    //  TODO: filter comments by user id. Make nickname be default author name.
     Query query = new Query("Comment").addSort(sortingParameter, sortingDirection);  
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
